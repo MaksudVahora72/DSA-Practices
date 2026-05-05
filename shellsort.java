4cfd25c21184964e40eb840dcfe187c4c5a1c885
MaksudVahora72 <maksud.vahora72@gmail.com>
2026-05-05 14:35:33 +0530

    commit-4

---
+++ shellsort.java
@@ -0,0 +1,29 @@
+public class shellsort {
+    public static void main(String[] args) {
+
+        int arr[] = {12, 34, 54, 2, 3};
+
+        sorting(arr);
+
+        for (int i : arr)
+            System.out.print(i + " ");
+
+    }
+
+    private static void sorting(int[] arr) {
+        int n = arr.length;
+
+        // Start with large gap, reduce it
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < n; i++) {
+                int temp = arr[i];
+                int j;
+
+                for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+                    arr[j] = arr[j - gap];
+                }
+                arr[j] = temp;
+            }
+        }
+    }
+}
